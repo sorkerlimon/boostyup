72088b142349c9e7ac93e842a56834228f9afc5c
sorkerlimon <sorkerlimon18@gmail.com>
2025-11-12 14:41:06 +0600

    create django project

---
+++ boostyup/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for boostyup project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boostyup.settings')
+
+application = get_asgi_application()
